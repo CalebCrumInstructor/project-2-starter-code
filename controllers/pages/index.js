b53adb28aead0c1d79127cd5d0b082683f87b650
ccrum292 <ccrum292@gmail.com>
2023-11-21 10:48:31 -0500

    update for controller

--- controllers/pages/index.js
+++ controllers/pages/index.js
@@ -13,7 +13,9 @@ router.use('/home', withAuth, home);
 router.use('/login', login);
 
 router.get('/', (req, res) => {
-  return res.render('welcome');
+  return res.render('welcome', {
+    logged_in: req.session.logged_in
+  });
 });
 
 module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,3 @@
+-- Change db name
 DROP DATABASE IF EXISTS project_2;
 CREATE DATABASE project_2;
