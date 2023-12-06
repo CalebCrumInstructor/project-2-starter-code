f05620680eda8e3d7d9a16ec426208d136754a0d
dfussell1 <daltonfussell1@gmail.com>
2023-12-05 21:41:04 -0600

    added functionality of multer and cloudinary as middleware for uploads

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const uploads = require('./middleware/multer')
+const cloudinary = require('./config/cloudinaryConfig')
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -34,6 +36,37 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public'));
+});
+
+app.post('/api/photo', uploads.single('file'), async (req, res) => {
+  console.log('Ahhhhhhhhhhhhh')
+  console.log(req.file)
+  try {
+    const cloudImgData = await cloudinary.uploader.upload('./uploads/' + req.file.filename, {
+      upload_preset: 'My Preset Here'
+    })
+
+    const imgInfoObj = {
+      public_id: cloudImgData.public_id,
+      filename: cloudImgData.original_filename,
+      url: cloudImgData.url
+    }
+
+    console.log(imgInfoObj);
+
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.json(err);
+  }
+});
+
+// app.post('/upload', multerUploads, (req, res) => {
+//   console.log('req.file :', req.file);
+//   });
 
 app.use(routes);
 
