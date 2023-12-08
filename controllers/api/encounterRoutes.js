ac4955ad436a6b4d6324954761d71ca220395b0d
josephdakotajohnson <josephdakotajohnson@gmail.com>
2023-12-07 21:59:57 -0600

    Fixed the user_id=NULL error with Caleb/Julie and worked on pulling the user stories on the Dashboard

--- controllers/api/encounterRoutes.js
+++ controllers/api/encounterRoutes.js
@@ -4,7 +4,11 @@ const { Story } = require('../../models');
 // /api/encounters - post
 router.post('/', async (req, res) => {
   try {
-    const storyData = await Story.create(req.body);
+    const storyData = await Story.create({
+      ...req.body,
+      user_id: req.session.user_id
+    });
+    console.log(req.body);
 
     res.status(200).json(storyData);
   } catch (err) {
