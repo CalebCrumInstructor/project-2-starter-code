7064de710c933a897a5fe96a76b4952393bbae51
JulieABowers <Haldirolorien@comcast.net>
2023-12-07 18:52:01 -0600

    Add lore pages

---
+++ controllers/pages/lore-page.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const { Lore } = require('../../models');
+
+// get all lore localhost/lore
+router.get('/', async (req, res) => {
+    try {
+        console.log('hit here')
+            // query db for user stories based on userId in req.session
+            // const data = await Lore.findOne({
+            //   where: {
+            //     id: req.session.user_id
+            //   }
+            // });
+
+        res.render('lore');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// get single lore /lore/2
+router.get('/:id', async (req, res) => {
+    try {
+        console.log(req.params.id)
+            // query db for user stories based on userId in req.session
+            const data = await Lore.findOne({
+              where: {
+                id: req.params.id
+              }
+            });
+
+        res.render('lore', {
+
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+module.exports = router;
